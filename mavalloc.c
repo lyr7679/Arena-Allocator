d6ba97eace44077c5ad252b954e368d53fc7ffc0
lyr7679 <otterball908@gmail.com>
2022-11-03 02:18:49 +0000

    starter variables

--- mavalloc.c
+++ mavalloc.c
@@ -1,17 +1,17 @@
 // The MIT License (MIT)
-// 
-// Copyright (c) 2021, 2022 Trevor Bakker 
-// 
+//
+// Copyright (c) 2021, 2022 Trevor Bakker
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES UTA OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,9 +22,24 @@
 
 #include "mavalloc.h"
 
+#define MAX_ALLOC 10000
+
+struct Node {
+  size_t size;
+  enum TYPE type;
+  void * arena;
+  int next;
+  int previous;
+};
+
+struct Node *arena_arr[MAX_ALLOC];
+
 int mavalloc_init( size_t size, enum ALGORITHM algorithm )
 {
-  return 0;
+    if(size < 0)
+        return -1;
+        
+    return 0;
 }
 
 void mavalloc_destroy( )
