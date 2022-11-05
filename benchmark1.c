c4a99f49e9470ba24cad9e13905adb8578dff30a
lyr7679 <otterball908@gmail.com>
2022-11-05 06:44:38 +0000

    my mavalloc broke the tests - benchmarks work fine with non-brroken code lol

--- benchmark1.c
+++ benchmark1.c
@@ -1,7 +1,21 @@
 #include "mavalloc.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+// This is the control benchmark to test the c malloc
+#define MAX 10
+int allocations[] = {0, 12, 65, 120, 1000, 2000, 5000, 10000, 65535, 128000};
 
 int main( int argc, char * argv[] )
 {
+  clock_t t;
+  for (int i = 0; i < MAX; i++) {
+    t = clock();
+    char *f = malloc(allocations[i]);
+    t = clock() - t;
+    free(f);
+    printf("Time for malloc of size %d to complete: %0.2f ms\n", allocations[i], (double)t);
+  }
 
   return 0;
 }
--- benchmark4.c
+++ benchmark4.c
@@ -1,7 +1,20 @@
 #include "mavalloc.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+// BEST FIT
+#define MAX 10
+int allocations[] = {0, 12, 65, 120, 1000, 2000, 5000, 10000, 65535, 128000};
 
 int main( int argc, char * argv[] )
 {
-
+  clock_t t;
+  for (int i = 0; i < MAX; i++) {
+    t = clock();
+    mavalloc_init(allocations[i], BEST_FIT);
+    t = clock() - t;
+    printf("Time for BEST FIT mavalloc of size %d to complete: %0.2f ms\n", allocations[i], (double)t);
+  }
+  
   return 0;
 }
