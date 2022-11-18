fa09802f91aee9a528bb44bfad8064501d8530da
ShoReagan <shoreaganw@gmail.com>
2022-11-17 19:33:41 -0600

    .

--- mavalloc.c
+++ mavalloc.c
@@ -68,6 +68,15 @@ int initialized = 0;
 int headIndex = 0;
 
 
+//Name:
+  //mavalloc_init()
+//Expected parameters:
+  //size_t, ALGORITHM
+//return values:
+  //int
+//Description:
+  //This function is used to allocate initial overall block of free space
+  //returns -1 on failure for any reason, and 0 for success
 int mavalloc_init( size_t size, enum ALGORITHM algorithm )
 {
     //clearing all global variables so when multiple tests are called
@@ -101,10 +110,19 @@ int mavalloc_init( size_t size, enum ALGORITHM algorithm )
     return 0;
 }
 
-//frees initially allocated address to clear array/linked list
+//Name:
+  //mavalloc_destroy()
+//Expected parameters:
+  //none
+//return values:
+  //VOID (none)
+//Description:
+  //This function is used to free initially allocated pointer address
+  //to discard the entire array/linked list after clearing it
 void mavalloc_destroy( )
 {
-  for(int i = 0; i <= indexx; i++) {
+  for(int i = 0; i <= indexx; i++) 
+  {
     arena_arr[i].arena = NULL;
     arena_arr[i].size = 0;
     arena_arr[i].type = 0;
@@ -115,18 +133,30 @@ void mavalloc_destroy( )
   return;
 }
 
+//Name:
+  //mavalloc_alloc()
+//Expected parameters:
+  //size_t
+//return values:
+  //VOID pointer
+//Description:
+  //This function is used to return the new created pointer of
+  //the desired allocated data after being given size
+  //ours uses a switch/case statement to determine which index is filled
 void * mavalloc_alloc( size_t size )
 {
     void * new_ptr = NULL;
     int indexOfHole;
     size = ALIGN4(size);
 
-    switch(curr_alg) {
+    switch(curr_alg) 
+    {
         case NEXT_FIT: 
         {
             indexOfHole = next_fit(size);
             nf_indexx = indexOfHole;
-            if(indexOfHole != -1) {
+            if(indexOfHole != -1) 
+            {
                 indexx++;
                 insertNode(indexOfHole, size);
                 new_ptr = arena_arr[indexx].arena;
@@ -136,7 +166,8 @@ void * mavalloc_alloc( size_t size )
         case FIRST_FIT:
         {
             indexOfHole = first_fit(size);
-            if(indexOfHole != -1) {
+            if(indexOfHole != -1) 
+            {
                 indexx++;
                 insertNode(indexOfHole, size);
                 new_ptr = arena_arr[indexx].arena;
@@ -146,7 +177,8 @@ void * mavalloc_alloc( size_t size )
         case BEST_FIT:
         {
           indexOfHole = best_fit(size);
-          if(indexOfHole != -1) { 
+          if(indexOfHole != -1) 
+          { 
             insertNode(indexOfHole, size);
             new_ptr = arena_arr[indexx].arena; 
           }    
@@ -155,7 +187,8 @@ void * mavalloc_alloc( size_t size )
         case WORST_FIT:
         {
           indexOfHole = worst_fit(size);
-          if(indexOfHole != -1) {
+          if(indexOfHole != -1) 
+          {
             insertNode(indexOfHole, size); 
             new_ptr = arena_arr[indexx].arena; 
           } 
@@ -166,10 +199,23 @@ void * mavalloc_alloc( size_t size )
   return new_ptr;
 }
 
+//Name:
+  //mavalloc_free()
+//Expected parameters:
+  //void pointer
+//return values:
+  //VOID (none)
+//Description:
+  //This function is used to free a node in the linked list
+  //simply by changing the type to 'H'
+  //passes to checkMerge function to make sure there are no
+  //holes next to eachother that neeed to be merged
 void mavalloc_free( void * ptr ) 
 {
-    for(int i = 0; i <= indexx; i++) {
-        if(arena_arr[i].arena == ptr) {
+    for(int i = 0; i <= indexx; i++) 
+    {
+        if(arena_arr[i].arena == ptr) 
+        {
             arena_arr[i].type = H;
             checkMerge(i);
             break;
@@ -178,23 +224,44 @@ void mavalloc_free( void * ptr )
   return;
 }
 
+//Name:
+  //mavalloc_size()
+//Expected parameters:
+  //none
+//return values:
+  //int
+//Description:
+  //simply increments through entire array and looks at all
+  //nonzero slots and returns that number
 int mavalloc_size( ) 
 {
   int number_of_nodes = 0;
-  for(int i = 0; i <= indexx; i++) {
+  for(int i = 0; i <= indexx; i++) 
+  {
     if(arena_arr[i].size > 0)
       number_of_nodes++;
   }
   return number_of_nodes;
 }
 
-//first fit: insert into first free node found
-//always start at beginning of table
+//Name:
+  //first_fit()
+//Expected parameters:
+  //size_t
+//return values:
+  //int
+//Description:
+  //This function is used to return the index of the array of the
+  //first free hole of available size
+  //increments relying on .next index value stored rather than
+  //incrementing sequentially
 int first_fit(size_t size)
 {
     int j = headIndex;
-    while(j < MAX_ALLOC && j != -1) {
-        if (arena_arr[j].type == H && arena_arr[j].size >= size) {
+    while(j < MAX_ALLOC && j != -1) 
+    {
+        if (arena_arr[j].type == H && arena_arr[j].size >= size) 
+        {
             return j;
         }
         else {
@@ -204,15 +271,25 @@ int first_fit(size_t size)
     return -1;
 }
 
-//check previous and next to see if there's a hole
-//if there is - merge (always merge into earlier node)
+//Name:
+  //checkMerge()
+//Expected parameters:
+  //int
+//return values:
+  //VOID (none)
+//Description:
+  //This function is used to check whether a node that has just been freed
+  //has a node before or after it that is also free
+  //if so, we merge the sizes into one large hole, but always into
+  //the previous node, rather than the next
 void checkMerge(int current)
 {
     int next = arena_arr[current].next;
     int previous = arena_arr[current].previous;
     int temp = 0;
     //combining if next node is also a hole
-    if(arena_arr[next].type == H) {
+    if(arena_arr[next].type == H) 
+    {
         //combing size into current node
         arena_arr[current].size += arena_arr[next].size;
         arena_arr[next].size = 0;
@@ -223,7 +300,8 @@ void checkMerge(int current)
         arena_arr[temp].previous = current;
     }
     //combining if previous node is also a hole
-    if(arena_arr[previous].type == H) {
+    if(arena_arr[previous].type == H) 
+    {
         arena_arr[previous].size += arena_arr[current].size;
         arena_arr[current].size = 0;
 
@@ -261,8 +339,10 @@ int next_fit(size_t size)
 
     //loop 2x bc we have to account for when next fit index isnt at root node
     while(count != 2) {
-        while(j < MAX_ALLOC && j != -1) {
-            if (arena_arr[j].type == H && arena_arr[j].size >= size) {
+        while(j < MAX_ALLOC && j != -1) 
+        {
+            if (arena_arr[j].type == H && arena_arr[j].size >= size) 
+            {
                 return j;
             }
             else
@@ -280,9 +360,12 @@ int best_fit(size_t size)
 {
   int bestFitIndex = -1;
   int temp1 = INT_MAX;
-  for(int i = 0; i <= indexx; i++) {
-    if(arena_arr[i].type == H && arena_arr[i].size >= size) {
-      if(temp1 > (arena_arr[i].size - size)) {
+  for(int i = 0; i <= indexx; i++) 
+  {
+    if(arena_arr[i].type == H && arena_arr[i].size >= size) 
+    {
+      if(temp1 > (arena_arr[i].size - size)) 
+      {
         temp1 = arena_arr[i].size - size;
         bestFitIndex = i;
       }
@@ -297,9 +380,12 @@ int worst_fit(size_t size)
 {
   int worstFitIndex = -1;
   int temp1 = 0;
-  for(int i = 0; i <= indexx; i++) {
-    if(arena_arr[i].type == H && arena_arr[i].size >= size) {
-      if(temp1 <= (arena_arr[i].size - size)) {
+  for(int i = 0; i <= indexx; i++) 
+  {
+    if(arena_arr[i].type == H && arena_arr[i].size >= size) 
+    {
+      if(temp1 <= (arena_arr[i].size - size)) 
+      {
         temp1 = arena_arr[i].size - size;
         worstFitIndex = i;
       }
@@ -326,7 +412,8 @@ void insertNode(int indexOfHole, size_t size)
   arena_arr[indexOfHole].arena = arena_arr[indexOfHole].arena + size;
   arena_arr[indexOfHole].previous = indexx;
 
-  if(arena_arr[indexx].previous == -1) {
+  if(arena_arr[indexx].previous == -1) 
+  {
     headIndex = indexx;
   }
 }
